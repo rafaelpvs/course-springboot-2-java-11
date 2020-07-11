516eb706e6a28379b81829ea34598841cbfb0e08
Rafael Pinto <rafael12wq@gmail.com>
2020-07-11 11:02:39 -0300

    Category

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -13,8 +13,6 @@ import javax.persistence.Table;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable{
